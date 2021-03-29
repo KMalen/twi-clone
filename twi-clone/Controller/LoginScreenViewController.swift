596f8f99526f43f9f417d72a9c5f3adf86f7a6e0
Kiryl Malenchik <kirillmalen@gmail.com>
2021-03-29 22:40:53 +0300

    add login screen add project hierarchy

---
+++ twi-clone/Controller/LoginScreenViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginScreenViewController.swift
+//  twi-clone
+//
+//  Created by Kiril Malenchik on 26.03.21.
+//
+
+import UIKit
+
+class LoginScreenViewController: UIViewController {
+    
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ twi-clone/Controller/StartViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  twi-clone
+//
+//  Created by Kiril Malenchik on 26.03.21.
+//
+
+import UIKit
+import FirebaseAuth
+
+class StartViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
+    @IBAction func unwindToStartView(_ unwindSegue: UIStoryboardSegue) {
+        //let sourceViewController = unwindSegue.source
+        // Use data from the view controller which initiated the unwind segue
+    }
+
+}
+
